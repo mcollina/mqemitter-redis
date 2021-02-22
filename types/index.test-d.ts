d9305018e55fc25c1ea82b0e30b6a0451b35d5a3
getlarge <getlarge@users.noreply.github.com>
2021-02-22 10:54:15 +0100

    fix: add types definitions (#37)
    * fix: add types def
    
    * fix: add types tests
    
    * fix: add extra tests

---
+++ types/index.test-d.ts
@@ -0,0 +1,41 @@
+import { expectError, expectType } from 'tsd';
+import mqEmitterRedis, { Message, MQEmitterRedis } from '.';
+
+expectType<MQEmitterRedis>(mqEmitterRedis());
+
+expectType<MQEmitterRedis>(
+  mqEmitterRedis({ concurrency: 200, matchEmptyLevels: true })
+);
+
+expectType<MQEmitterRedis>(
+  mqEmitterRedis({
+    concurrency: 10,
+    matchEmptyLevels: true,
+    separator: '/',
+    wildcardOne: '+',
+    wildcardSome: '#',
+  })
+);
+
+expectType<MQEmitterRedis>(
+  mqEmitterRedis({
+    concurrency: 10,
+    matchEmptyLevels: true,
+    host: 'localhost',
+    port: 6379,
+    reconnectOnError: (error: Error) => true,
+    retryStrategy: (times: number) => times * 1.5,
+  })
+);
+
+function listener(message: Message, done: () => void) {}
+
+expectType<MQEmitterRedis>(mqEmitterRedis().on('topic', listener));
+
+expectType<void>(mqEmitterRedis().removeListener('topic', listener));
+
+expectError(mqEmitterRedis().emit(null));
+
+expectType<void>(mqEmitterRedis().emit({ topic: 'test', prop1: 'prop1' }));
+
+expectType<void>(mqEmitterRedis().close(() => null));
