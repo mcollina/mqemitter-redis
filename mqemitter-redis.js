fee02b7cd95f60b653ed36fc65d09fa5b25dea78
pelicarno <peter.pelicarno@gmail.com>
2021-09-06 13:17:18 +0200

    Prevent throwing error and enable redis to retry to reconnect

--- mqemitter-redis.js
+++ mqemitter-redis.js
@@ -71,7 +71,10 @@ function MQEmitterRedis (opts) {
   })
 
   this.subConn.on('error', function (err) {
-    that._onError(err)
+    // redis can retry to make a connection
+    if (err.code !== 'ECONNREFUSED') {
+      that._onError(err)
+    }
   })
 
   this.pubConn.on('connect', function () {
@@ -79,7 +82,10 @@ function MQEmitterRedis (opts) {
   })
 
   this.pubConn.on('error', function (err) {
-    that._onError(err)
+    // redis can retry to make a connection
+    if (err.code !== 'ECONNREFUSED') {
+      that._onError(err)
+    }
   })
 
   MQEmitter.call(this, opts)
