f31a0c39a6751da83582ba98f7130c7b8f01e613
Matteo Collina <hello@matteocollina.com>
2025-07-14 22:50:46 +0200

    Use namespace to correctly support the new strip types
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,42 +1,44 @@
 import type { RedisOptions } from 'ioredis'
 import type { MQEmitter } from 'mqemitter'
 
-export interface MQEmitterOptions {
-  concurrency?: number;
-  matchEmptyLevels?: boolean;
-  separator?: string;
-  wildcardOne?: string;
-  wildcardSome?: string;
-  connectionString?: string;
-}
+declare namespace MQEmitterRedis {
+  export interface MQEmitterOptions {
+    concurrency?: number;
+    matchEmptyLevels?: boolean;
+    separator?: string;
+    wildcardOne?: string;
+    wildcardSome?: string;
+    connectionString?: string;
+  }
 
-export interface LRUCacheOptions {
-  ttlLRUCache?: number;// Time to live for the LRU cache in milliseconds
-  maxLRUCache?: number;// Maximum number of items in the LRU cache
-}
+  export interface LRUCacheOptions {
+    ttlLRUCache?: number;// Time to live for the LRU cache in milliseconds
+    maxLRUCache?: number;// Maximum number of items in the LRU cache
+  }
 
-export type Message = Record<string, any> & { topic: string }
+  export type Message = Record<string, any> & { topic: string }
 
-export interface MQEmitterRedis extends MQEmitter {
-  new (options?: MQEmitterOptions & RedisOptions & LRUCacheOptions): MQEmitterRedis;
-  current: number;
-  concurrent: number;
-  on(
-    topic: string,
-    listener: (message: Message, done: () => void) => void,
-    callback?: () => void
-  ): this;
-  emit(message: Message, callback?: (error?: Error) => void): void;
-  removeListener(
-    topic: string,
-    listener: (message: Message, done: () => void) => void,
-    callback?: () => void
-  ): void;
-  close(callback: () => void): void;
+  export interface MQEmitterRedis extends MQEmitter {
+    new (options?: MQEmitterOptions & RedisOptions & LRUCacheOptions): MQEmitterRedis;
+    current: number;
+    concurrent: number;
+    on(
+      topic: string,
+      listener: (message: Message, done: () => void) => void,
+      callback?: () => void
+    ): this;
+    emit(message: Message, callback?: (error?: Error) => void): void;
+    removeListener(
+      topic: string,
+      listener: (message: Message, done: () => void) => void,
+      callback?: () => void
+    ): void;
+    close(callback: () => void): void;
+  }
 }
 
 declare function MQEmitterRedis (
-  options?: MQEmitterOptions & RedisOptions & LRUCacheOptions
-): MQEmitterRedis
+  options?: MQEmitterRedis.MQEmitterOptions & RedisOptions & MQEmitterRedis.LRUCacheOptions
+): MQEmitterRedis.MQEmitterRedis
 
-export default MQEmitterRedis
+export = MQEmitterRedis
